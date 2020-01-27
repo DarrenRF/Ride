878ab34f05862b3166062cab39d6dc3b31a2311d
Darren Fuller <darrenfuller@Darren.local>
2020-01-26 16:35:49 -0800

    first commit

---
+++ ride/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ride
+//
+//  Created by Darren Fuller on 1/26/20.
+//  Copyright © 2020 Darren Fuller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
